4dfefc3c21f2328a2f0a910bdc3a175fa591a3e2
Dmitrii <kopenkin.da@gmail.com>
2023-02-15 14:51:12 +0100

    Setup eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,23 @@
  * @type {import("eslint").Linter.Config}
  */
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["plugin:@typescript-eslint/recommended"],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-        disallowTypeAnnotations: true,
-      },
-    ],
-    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-  },
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: "./tsconfig.json",
+	},
+	plugins: ["@typescript-eslint"],
+	extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+	rules: {
+		semi: "error",
+		"prefer-const": "error",
+		"@typescript-eslint/consistent-type-imports": [
+			"error",
+			{
+				prefer: "type-imports",
+				fixStyle: "separate-type-imports",
+				disallowTypeAnnotations: true,
+			},
+		],
+		"@typescript-eslint/consistent-type-definitions": ["error", "type"],
+	},
 };
